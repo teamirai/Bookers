89c69b7a87cd6e6bd0b77e5ac13b16833c8a28ee
EC2 Default User <ec2-user@ip-172-31-6-163.ap-northeast-1.compute.internal>
2023-05-23 12:38:30 +0000

    [Finish]削除機能を作成しよう

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
  
 root :to => 'homes#home'
 resources :books
-post 'books' => 'books#create' 
-get 'books' => 'books#index'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.htm
 end
 
